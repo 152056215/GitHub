81149c3d5b285059f3722b34c62469345a85c885
152056215 <31466858+152056215@users.noreply.github.com>
2017-10-13 21:30:11 +0800

    Update Calculator_0.cs

--- Calculator_0.cs
+++ Calculator_0.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ConsoleApplication1
 {
@@ -6,48 +6,56 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Calculator cal = new Calculator();
-            cal.math();
+            Calculator_0 c = new Calculator_0();
+            Console.WriteLine("请输入两个整数：");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            c.math(x, y);
+            Console.WriteLine("请输入两个字符串：");
+            string m = Console.ReadLine();
+            string n = Console.ReadLine();
+            c.math(m, n);
         }
     }
-    public class Calculator
+
+    public class Calculator_0
     {
-        Boolean w = true;
-        public void math()
+        public void math(int a,int b)
         {
-            while (w)
+            Console.WriteLine("请选择运算符{1.+，2.-，3.*，4.÷}：");
+            int c = Convert.ToInt32(Console.ReadLine());
+            switch (c)
             {
-                Console.WriteLine("请输入两个运算数：");
-                int a = Convert.ToInt32(Console.ReadLine());
-                int b = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("请选择运算符{1.+，2.-，3.*，4.÷}：");
-                int c = Convert.ToInt32(Console.ReadLine());
-                switch (c)
-                {
-                    case 1: Console.WriteLine(a + b);
-                        break;
-                    case 2: Console.WriteLine(a - b);
-                        break;
-                    case 3: Console.WriteLine(a * b);
-                        break;
-                    case 4:
-                        {
-                            try { int s = a / b; Console.WriteLine(s); }
-                            catch (Exception e) { Console.WriteLine("分母不能为0！"); }
-                        }; break;
-                }
-                Console.WriteLine("是否继续运算？\n 1.yes    2.no  ");
-                string d = Console.ReadLine();
-                if (d.Equals("1"))
-                {
-                    w = true;
-                }
-                else if (d.Equals("2"))
-                {
-                    w = false;
-                }
+                case 1: Console.WriteLine(a + b);
+                    break;
+                case 2: Console.WriteLine(a - b);
+                    break;
+                case 3: Console.WriteLine(a * b);
+                    break;
+                case 4:
+                    {
+                        try { int s = a / b; Console.WriteLine(s); }
+                        catch (Exception e) { Console.WriteLine("分母不能为0！"); }
+                    }; break;
             }
         }
-        
+        public void math(string c, string d)
+        {
+            Console.WriteLine("请选择运算符{1.+，2.-}：");
+            int i = Convert.ToInt32(Console.ReadLine());
+            switch (i)
+            {
+                case 1: Console.WriteLine(c+d);
+                    break;
+                case 2:
+                    {
+                        if (c.Contains(d))  //c中是否包含d,若是则用Replace(string old,string new)方法替换成新字符串
+                        {
+                            Console.WriteLine(c.Replace(d,""));
+                        }
+                    }
+                    break;
+            }
+        }
     }
 }
